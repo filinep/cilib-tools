a14998fa3bc79acaa093801681c3fdb7ce7b6063
filinep <filinep@d0bebf6c-90b9-c7dd-0b15-95bff7c12ae8>
2011-06-19 17:27:43 +0000

    fixed some readability stuff and added license stuff to some files

--- cilib-tools/common.py
+++ cilib-tools/common.py
@@ -1,12 +1,43 @@
+"""
+ * cilib-tools
+ * Copyright (C) 2011
+ * Filipe Nepomuceno
+ *
+ * These tools are free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * These tools are distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ """
 import copy
 from gi.repository import Gtk
 from xml.etree import ElementTree
 from cilibReflection import *
 
-sections = {"algorithm":None, "problem":None, "measurements":None, "simulation":None}
+header = """<?xml version="1.0"?>
+<!DOCTYPE simulator [
+<!ATTLIST algorithm id ID #IMPLIED>
+<!ATTLIST problem id ID #IMPLIED>
+<!ATTLIST measurements id ID #IMPLIED>
+]>\n\n"""
+
+sections = {
+    'algorithm':None,
+    'problem':None,
+    'measurements':None,
+    'simulation':None
+    }
+
 statusbar = None
 window = None
-tabs = ""
+tabs = ''
 
 def set_status(string):
     if statusbar is not None:
@@ -14,30 +45,27 @@ def set_status(string):
 
 def set_title(string):
     if window is not None:
-        window.set_title("CILib-Tools - " + string)
+        window.set_title('CILib-Tools - ' + string)
 
 def save_xml(outFile, alg=None, prob=None, m=None, sim=None):
     global sections
     if not None in [alg, prob, m, sim]:
-        individuals = {"algorithm": find_idref(sections["algorithm"].store, alg),
-                       "problem": find_idref(sections["problem"].store, prob),
-                       "measurements": find_idref(sections["measurements"].store, m),
-                       "simulation": sim}
+        individuals = {
+            'algorithm': find_idref(sections['algorithm'].store, alg),
+            'problem': find_idref(sections['problem'].store, prob),
+            'measurements': find_idref(sections['measurements'].store, m),
+            'simulation': sim
+            }
 
     keys = sorted(sections.keys())
 
-    outFile.write("""<?xml version="1.0"?>\n""")
-    outFile.write("""<!DOCTYPE simulator [\n""")
-    outFile.write("""<!ATTLIST algorithm id ID #IMPLIED>\n""")
-    outFile.write("""<!ATTLIST problem id ID #IMPLIED>\n""")
-    outFile.write("""<!ATTLIST measurements id ID #IMPLIED>\n""")
-    outFile.write("""]>\n\n""")
-    outFile.write("""<simulator>\n""")
+    outFile.write(header)
+    outFile.write('<simulator>\n')
 
     for el in keys:
         inc_tab()
-        if not el == "measurements":
-            outFile.write("\t<" + el + "s>\n")
+        if not el == 'measurements':
+            outFile.write('\t<' + el + 's>\n')
             inc_tab()
 
         if not None in [alg, prob, m, sim]:
@@ -48,67 +76,65 @@ def save_xml(outFile, alg=None, prob=None, m=None, sim=None):
                 recurse_save(outFile, sections[el].store, root, el)
                 root = sections[el].store.iter_next(root)
 
-        if not el == "measurements":
-            outFile.write("\t</" + el + "s>\n")
+        if not el == 'measurements':
+            outFile.write('\t</' + el + 's>\n')
             dec_tab()
         dec_tab()
 
-    outFile.write("""</simulator>\n""")
+    outFile.write('''</simulator>\n''')
     outFile.close()
 
 def recurse_save(f, src, root, el):
-    if el == "simulation":
+    if el == 'simulation':
         row = src.get(root, 0, 1, 2, 3, 4)
-        f.write(tab() + "<simulation samples=\"" + row[3] + "\">\n")
-        f.write(tab() + "\t<algorithm idref=\"" + row[0] + "\"/>\n")
-        f.write(tab() + "\t<problem idref=\"" + row[1] + "\"/>\n")
-        f.write(tab() + "\t<measurements idref=\"" + row[2] + "\"/>\n")
-        f.write(tab() + "\t<output format=\"TXT\" file=\"" + row[4] + "\"/>\n")
-        f.write(tab() + "</simulation>\n")
+        f.write(tab() + '<simulation samples=\"' + row[3] + '\">\n')
+        f.write(tab() + '\t<algorithm idref=\"' + row[0] + '\"/>\n')
+        f.write(tab() + '\t<problem idref=\"' + row[1] + '\"/>\n')
+        f.write(tab() + '\t<measurements idref=\"' + row[2] + '\"/>\n')
+        f.write(tab() + '\t<output format=\"TXT\" file=\"' + row[4] + '\"/>\n')
+        f.write(tab() + '</simulation>\n')
     else:
         row = src.get(root, 0, 1, 2, 3, 4, 5, 6)
-        f.write(tab() + "<" + str(row[0]))
+        f.write(tab() + '<' + str(row[0]))
 
-        if not row[5] == "":
-            f.write(" id=\"" + str(row[5]) + "\"")
+        if not row[5] == '':
+            f.write(' id=\"' + str(row[5]) + '\"')
 
-        if row[3] == "value":
-            f.write(" value=\"" + str(row[2]) + "\"")
-        elif row[3] == "":
+        if row[3] == 'value':
+            f.write(' value=\"' + str(row[2]) + '\"')
+        elif row[3] == '':
             pass
-        elif row[3] == "idref":
-            f.write(" idref=\"" + str(row[2]) + "\"")
-        elif row[3] == "file":
-            f.write(" format=\"TXT\" file=\"" + str(row[2]) + "\"")
+        elif row[3] == 'idref':
+            f.write(' idref=\"' + str(row[2]) + '\"')
         else:
-            f.write(" class=\"" + str(row[2]) + "\"")
+            f.write(' class=\"' + str(row[2]) + '\"')
 
         #check if must go to sublevel
         allDefault = True
         for i in range(src.iter_n_children(root)):
             lmnt = src.get_value(src.iter_nth_child(root, i), 2)
-            if not (lmnt == "Default" or lmnt == "Primitive" or lmnt == "idref" or lmnt == "file"):
+            if not (lmnt == 'Default' or lmnt == 'Primitive' or lmnt == 'idref'):
                 allDefault = False
 
         if not allDefault:
-            f.write(">\n")
+            f.write('>\n')
 
             for i in range(src.iter_n_children(root)):
                 child = src.iter_nth_child(root, i)
                 lmnt = src.get(child, 2)[0]
 
-                if not (lmnt == "Default" or lmnt == "Primitive"):
+                if not (lmnt == 'Default' or lmnt == 'Primitive'):
                     inc_tab()
                     recurse_save(f, src, src.iter_nth_child(root, i), el)
                     dec_tab()
 
-            f.write(tab() + "</" + str(row[0]) + ">\n")
+            f.write(tab() + '</' + str(row[0]) + '>\n')
         else:
-            f.write("/>\n")
+            f.write('/>\n')
 
 def inc_tab():
     global tabs
-    tabs = tabs + "\t"
+    tabs = tabs + '\t'
 
 def dec_tab():
     global tabs
@@ -146,12 +172,12 @@ def find_in_store(store, it, column, term):
     return None
 
 def open_xml(f):
-    xmlFile = open(f, "rw").read()
+    xmlFile = open(f, 'rw').read()
     xml = ElementTree.fromstring(xmlFile)
 
     #populate the trees
     for c in xml:
-        if c.tag == "measurements":
+        if c.tag == 'measurements':
             recurse_open(c, None, sections[c.tag].store, c.tag)
         else:
             for cc in c:
@@ -161,23 +187,23 @@ def recurse_open(xmlElement, modelElement, model, el):
     global sections
     keys = sorted(sections.keys())
 
-    if el == "simulation":
-        if "samples" in xmlElement.attrib.keys():
-            sam = xmlElement.attrib["samples"]
+    if el == 'simulation':
+        if 'samples' in xmlElement.attrib.keys():
+            sam = xmlElement.attrib['samples']
 
         for e in xmlElement:
-            if e.tag == "algorithm":
-                alg = e.attrib["idref"]
-            elif e.tag == "problem":
-                prob = e.attrib["idref"]
-            elif e.tag == "measurements":
-                m = e.attrib["idref"]
-            elif e.tag == "output":
-                f = e.attrib["file"]
-            elif e.tag == "samples":
-                sam = e.attrib["value"]
+            if e.tag == 'algorithm':
+                alg = e.attrib['idref']
+            elif e.tag == 'problem':
+                prob = e.attrib['idref']
+            elif e.tag == 'measurements':
+                m = e.attrib['idref']
+            elif e.tag == 'output':
+                f = e.attrib['file']
+            elif e.tag == 'samples':
+                sam = e.attrib['value']
 
-        model.append([alg, prob, m, sam, f, True, "#ffffff", True,
+        model.append([alg, prob, m, sam, f, True, '#ffffff', True,
                      sections[el].comboModel, sections[el].comboModel,
                      sections[el].comboModel])
     else:
@@ -185,30 +211,30 @@ def recurse_open(xmlElement, modelElement, model, el):
 
         #get info to add to model
         #output is special: has more than 1 attribute
-        if xmlElement.tag == "output":
-            toInsert.append(xmlElement.attrib["file"])
-            toInsert.append("file")
-            toInsert.append("primitive")
-            toInsert.append("")
+        if xmlElement.tag == 'output':
+            toInsert.append(xmlElement.attrib['file'])
+            toInsert.append('file')
+            toInsert.append('primitive')
+            toInsert.append('')
             toInsert.append(True)
         elif len(xmlElement.attrib.keys()) > 0:
-            ID = ""
+            ID = ''
 
             #iterate over remaining attributes
             for k in xmlElement.attrib.keys():
                 #save id for later
-                if k == "id":
+                if k == 'id':
                     ID = xmlElement.attrib[k]
                     sections[xmlElement.tag].items.append(ID)
 
                 #value and idref are primitives
-                elif k == "idref" or k == "value":
+                elif k == 'idref' or k == 'value':
                     toInsert.append(xmlElement.attrib[k])
                     toInsert.append(k)
-                    toInsert.append("primitive")
+                    toInsert.append('primitive')
 
                 #for class need to get the default base class...
-                elif k == "class":
+                elif k == 'class':
                     toInsert.append(xmlElement.attrib[k])
                     toInsert.append(k)
 
@@ -217,42 +243,45 @@ def recurse_open(xmlElement, modelElement, model, el):
                         default = sections[xmlElement.tag].base
                     else:
                         parent = model.get_value(modelElement, 2)
-                        methods = cilib.getMethods("net.sourceforge.cilib." + parent)
+                        methods = cilib.getMethods('net.sourceforge.cilib.' + parent)
 
-                        index = methods["methods"].index(xmlElement.tag)
-                        default = methods["parameters"][index][0]
+                        index = methods['methods'].index(xmlElement.tag)
+                        default = methods['parameters'][index][0]
 
-                    toInsert.append(default.replace("net.sourceforge.cilib.", ""))
+                    toInsert.append(default.replace('net.sourceforge.cilib.', ''))
 
-                #assume the rest of the attributes can become method calls, so add them as children
+                #assume the rest of the attributes can become method calls,
+                #so add them as children
                 else:
                     element = ElementTree.Element(k)
-                    element.set("value", xmlElement.attrib[k])
+                    element.set('value', xmlElement.attrib[k])
                     xmlElement.append(element)
                     del(xmlElement.attrib[k])
 
             if len(xmlElement.attrib) == 0:
-                toInsert.append("")
-                toInsert.append("")
-                toInsert.append("")
+                toInsert.append('')
+                toInsert.append('')
+                toInsert.append('')
 
             toInsert.append(ID)
             toInsert.append(True)
 
-        #this is for elements without any info e.g. <simulation>... well that's it i guess
+        #this is for elements without any info e.g. <simulation>...
+        #well that's it i guess
         else:
-            toInsert.append("")
-            toInsert.append("")
-            toInsert.append("")
-            toInsert.append("")
+            toInsert.append('')
+            toInsert.append('')
+            toInsert.append('')
+            toInsert.append('')
             toInsert.append(True)
 
         #add to model
         modelChild = model.append(modelElement, toInsert)
 
         #get available options
-        if "class" in xmlElement.attrib.keys():
-            options = copy.deepcopy(cilib.getMethods("net.sourceforge.cilib." + xmlElement.attrib["class"]))
+        if 'class' in xmlElement.attrib.keys():
+            options = copy.deepcopy(cilib.getMethods('net.sourceforge.cilib.'
+                                                + xmlElement.attrib['class']))
         else:
             options = None
 
@@ -261,27 +290,28 @@ def recurse_open(xmlElement, modelElement, model, el):
             #keep track of used options
             if options is not None:
                 #remove used options, except add* methods cos can have more than 1
-                if not e.tag.startswith("add") and e.tag in options["methods"]:
-                    del(options["parameters"][options["methods"].index(e.tag)])
-                    del(options["methods"][options["methods"].index(e.tag)])
+                if not e.tag.startswith('add') and e.tag in options['methods']:
+                    del(options['parameters'][options['methods'].index(e.tag)])
+                    del(options['methods'][options['methods'].index(e.tag)])
 
             recurse_open(e, modelChild, model, el)
 
         #the rest of the options not in the xml file
         if options is not None:
-            for m in range(len(options["methods"])):
-                toInsert = [options["methods"][m], Gtk.ListStore(str)]
+            for m in range(len(options['methods'])):
+                toInsert = [options['methods'][m], Gtk.ListStore(str)]
 
-                if not options["parameters"][m][0] == "primitive":
-                    toInsert.append("Default")
-                    toInsert.append("class")
-                    toInsert.append(options["parameters"][m][0].replace("net.sourceforge.cilib.", ""))
+                if not options['parameters'][m][0] == 'primitive':
+                    toInsert.append('Default')
+                    toInsert.append('class')
+                    toInsert.append(options['parameters'][m][0]
+                                        .replace('net.sourceforge.cilib.', ''))
                 else:
-                    toInsert.append("Primitive")
-                    toInsert.append("value")
-                    toInsert.append("primitive")
+                    toInsert.append('Primitive')
+                    toInsert.append('value')
+                    toInsert.append('primitive')
 
-                toInsert.append("")
+                toInsert.append('')
                 toInsert.append(True)
                 model.append(modelChild, toInsert)
 
--- cilib-tools/simulator.py
+++ cilib-tools/simulator.py
@@ -1,3 +1,21 @@
+"""
+ * cilib-tools
+ * Copyright (C) 2011
+ * Filipe Nepomuceno
+ *
+ * These tools are free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * These tools are distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ """
 import sys
 import os
 import tempfile
@@ -12,37 +30,34 @@ class Simulator(Gtk.Box):
     def __init__(self):
         super(Simulator, self).__init__()
         self.gui = Gtk.Builder()
-        self.gui.add_from_file("../etc/ui/simulator.glade")
+        self.gui.add_from_file('../etc/ui/simulator.glade')
 
-        self.pack_start(self.get("simulator"), True, True, 0)
+        self.pack_start(self.get('simulator'), True, True, 0)
         self.gui.connect_signals(self)
 
         self.vte = Vte.Terminal()
-        pane = self.get("vteScroll")
+        pane = self.get('vteScroll')
         pane.add(self.vte)
 
         self.working = None
-        self.pause = False
-
-        self.store = self.get("store")
-        self.get("visibilityFilter").set_visible_column(7)
-
-        self.comboModel = self.get("comboModel")
+        self.store = self.get('store')
+        self.get('visibilityFilter').set_visible_column(7)
+        self.comboModel = self.get('comboModel')
 
     def get(self, name):
         return self.gui.get_object(name)
 
     def combo_edited(self, column, path, new_text):
-        treeIter = self.store.get_iter(path)
+        it = [i for i in self.store if i[7]][int(path)]
 
         if type(new_text) == Gtk.TreeIter:
             new_text = self.comboModel.get_value(new_text, 0)
 
-        self.store.set_value(treeIter, self.get("treeview").get_columns().index(column) - 1, new_text)
+        it[self.get('treeview').get_columns().index(column) - 1] = new_text
 
     def entry_edited(self, column, path, new_text):
-        treeIter = self.store.get_iter(path)
-        self.store.set_value(treeIter, self.get("treeview").get_columns().index(column) - 1, new_text)
+        it = [i for i in self.store if i[7]][int(path)]
+        it[self.get('treeview').get_columns().index(column) - 1] = new_text
 
     def populate_combo(self, column, editable, path):
         self.comboModel.clear()
@@ -51,19 +66,20 @@ class Simulator(Gtk.Box):
             self.comboModel.append([i])
 
     def on_add_click(self, button):
-        self.store.append(["", "", "", "30", "", True, "#ffffff", True,
+        self.store.append(['', '', '', '30', '', True, '#ffffff', True,
                            self.comboModel, self.comboModel, self.comboModel])
 
     def on_outputfolder_set(self, widget):
-        print widget.get_filename()
+        newFolder = widget.get_filename()
+        for i in self.store:
+            i[4].split('/')
 
     def on_header_toggle(self, w):
-        newState = not self.get("runHeader").get_active()
-        self.get("runHeader").set_active(newState)
+        newState = not self.get('runHeader').get_active()
+        self.get('runHeader').set_active(newState)
         self.store.foreach(self.toggle_all, newState)
 
     def toggle_all(self, model, path, it, data):
-        it = self.store.get_iter(path)
         if self.store.get_value(it, 7):
             self.store.set_value(it, 5, data)
 
@@ -74,9 +90,9 @@ class Simulator(Gtk.Box):
     def on_stop_click(self, button):
         try:
             os.kill(self.working[1], signal.SIGKILL)
-            self.working = "Done"
+            self.working = 'Done'
         except Exception:
-            Common.set_status("No simulations running")
+            Common.set_status('No simulations running')
 
     def on_run_click(self, button):
         if self.working is None:
@@ -85,12 +101,14 @@ class Simulator(Gtk.Box):
             self.store.foreach(self.prepare_run, pids)
             self.collective_run(pids)
         else:
-            Common.set_status("Simulations are already running...")
+            Common.set_status('Simulations are already running...')
 
     def collective_run(self, pids):
         for f in pids:
-            if not self.working == "Done":
-                self.send_text("\rRunning simulation " + str(pids.index(f) + 1) + " of " + str(len(pids)) + ": " + f[1] + " " + f[2] + " " + f[3])
+            if not self.working == 'Done':
+                self.send_text('\rRunning simulation ' + str(pids.index(f) + 1)
+                               + ' of ' + str(len(pids)) + ': ' + f[1] + ' '
+                               + f[2] + ' ' + f[3])
                 self.working = self.start_sim(f[0])
                 working = True
 
@@ -106,8 +124,8 @@ class Simulator(Gtk.Box):
             else:
                 break
 
-        if self.working == "Done":
-            self.send_text("\r\nSimulations aborted")
+        if self.working == 'Done':
+            self.send_text('\r\nSimulations aborted')
         self.working = None
 
     def prepare_run(self, model, path, it, data):
@@ -118,7 +136,7 @@ class Simulator(Gtk.Box):
 
     def create_tempfile(self, it):
         row = self.store.get(it, 0, 1, 2)
-        outFile = tempfile.NamedTemporaryFile(mode="r+", delete=False)
+        outFile = tempfile.NamedTemporaryFile(mode='r+', delete=False)
 
         Common.save_xml(outFile, row[0], row[1], row[2], it)
 
@@ -126,20 +144,19 @@ class Simulator(Gtk.Box):
 
     def check_folder(self):
         for i in self.store:
-            f = os.path.join(self.get("outputFolder").get_filename(), i[4])
+            f = os.path.join(self.get('outputFolder').get_filename(), i[4])
             if os.path.exists(f):
-                i[6] = "#00dd00"
-
-    def restart_vte(self, widget):
-        self.vte.fork_command_full(Vte.PtyFlags.DEFAULT, "~", ["/bin/bash"], [],
-                                GLib.SpawnFlags.CHILD_INHERITS_STDIN, None, None)
+                i[6] = '#00dd00'
 
     def start_sim(self, sim):
-        return self.vte.fork_command_full(Vte.PtyFlags.DEFAULT, ".", ["../bin/simulator.sh", sim], [], GLib.SpawnFlags.CHILD_INHERITS_STDIN, None, None)
+        return self.vte.fork_command_full(Vte.PtyFlags.DEFAULT, '.',
+                                          ['../bin/simulator.sh', sim], [], 
+                                          GLib.SpawnFlags.CHILD_INHERITS_STDIN,
+                                          None, None)
 
     def send_text(self, text):
-        if not text[-1] == "\n":
-            text = text + "\n"
+        if not text[-1] == '\n':
+            text = text + '\n'
 
         self.vte.feed(text, len(text))
 
@@ -150,11 +167,11 @@ class Simulator(Gtk.Box):
             row = ' '.join([r[i] for i in range(5)])
             for w in words:
                 found = found and (row.find(w) > -1)
-            r[7] = (found or editable.get_text() == "")
+            r[7] = (found or editable.get_text() == '')
 
     def clear_search_click(self, entry, icon, data):
         if icon == Gtk.EntryIconPosition.SECONDARY:
-            entry.set_text("")
+            entry.set_text('')
 
     def on_dimension_remove_click(self, treeSelection):
         data = treeSelection.get_selected()
@@ -162,24 +179,24 @@ class Simulator(Gtk.Box):
             data[0].remove(data[1])
 
     def on_dimension_add_click(self, a1):
-        self.get("dimGenModel").append(["30"])
+        self.get('dimGenModel').append(['30'])
 
     def on_generate_simulations_click(self, button):
         #TODO read probs, TODO constraints
-        for el in ["algorithm", "problem", "measurements"]:
-            model = self.get(el + "GenModel")
+        for el in ['algorithm', 'problem', 'measurements']:
+            model = self.get(el + 'GenModel')
             model.clear()
             for i in Common.sections[el].items:
                 model.append([True, i])
 
-        dialog = self.get("simGeneratorDialog")
+        dialog = self.get('simGeneratorDialog')
         response = dialog.run()
         dialog.hide()
 
-        algs = [i for i in self.get("algorithmGenModel") if i[0]]
-        probs = [i for i in self.get("problemGenModel") if i[0]]
-        ms = [i for i in self.get("measurementsGenModel") if i[0]]
-        dims = [i for i in self.get("dimGenModel")]
+        algs = [i for i in self.get('algorithmGenModel') if i[0]]
+        probs = [i for i in self.get('problemGenModel') if i[0]]
+        ms = [i for i in self.get('measurementsGenModel') if i[0]]
+        dims = [i for i in self.get('dimGenModel')]
 
         if response == 0:
             #TODO setup probs
@@ -187,14 +204,18 @@ class Simulator(Gtk.Box):
                 for d in dims:
                     for a in algs:
                         for p in probs:
-                            self.store.append([a[1], m[1], d[0]+"_"+p[1], self.get("samplesEntry").get_text(),
-                                "data/" + d[0] + "_" + p[1] + "_" + a[1] + ".txt", True, "#ffffff", True,
-                                self.comboModel, self.comboModel, self.comboModel])
+                            self.store.append([a[1], m[1], d[0]+'_'+p[1],
+                                              self.get('samplesEntry').get_text(),
+                                              'data/' + d[0] + '_' + p[1] + '_'
+                                              + a[1] + '.txt', True, '#ffffff', True,
+                                              self.comboModel, self.comboModel,
+                                              self.comboModel])
 
     def dim_edited(self, cell, path, newText):
-        model = self.get("dimGenModel")
+        model = self.get('dimGenModel')
         model.set_value(model.get_iter(path), 0, newText)
 
     def on_gen_toggle(self, model, path):
         it = model.get_iter(path)
         model.set_value(it, 0, not model.get_value(it, 0))
+
